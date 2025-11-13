444b258488b482cbeaee7067e77b1506f8f2b624
BaiqWinataRahmadena <baiqwinatarahmadena@gmail.com>
2025-11-13 19:13:49 +0800

    Fix(AppServiceProvider): Remove non-existent TLS header

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Http\Request; // <-- Pastikan ini ada
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // MEMPERCAYAI PROXY RENDER (Versi yang sudah diperbaiki)
+        // Kita hapus HEADER_X_FORWARDED_TLS yang error
+        Request::setTrustedProxies(
+            ['*'], // Percayai semua proxy
+            Request::HEADER_X_FORWARDED_FOR |
+            Request::HEADER_X_FORWARDED_HOST |
+            Request::HEADER_X_FORWARDED_PORT |
+            Request::HEADER_X_FORWARDED_PROTO |
+            Request::HEADER_X_FORWARDED_PREFIX
+        );
     }
 }
